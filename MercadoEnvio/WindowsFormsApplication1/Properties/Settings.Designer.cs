72c3d404eafb284ac8e8dbc57e86bbfade4c0921
FBerco <berco.fede@gmail.com>
2016-06-05 18:28:47 -0300

    Configuro la conección a la base de datos

--- MercadoEnvio/WindowsFormsApplication1/Properties/Settings.Designer.cs
+++ MercadoEnvio/WindowsFormsApplication1/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace GDD.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLSERVER2012;Initial Catalog=GD1C2016;Integrated Security=" +
+            "True")]
+        public string GD1C2016ConnectionString {
+            get {
+                return ((string)(this["GD1C2016ConnectionString"]));
+            }
+        }
     }
 }

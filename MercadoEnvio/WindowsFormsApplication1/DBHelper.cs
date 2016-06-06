269307199f6c4f2a29e77c3857f19ff97b0bcf44
FBerco <berco.fede@gmail.com>
2016-06-06 20:34:23 -0300

    Agrego DBHelper

---
+++ MercadoEnvio/WindowsFormsApplication1/DBHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GDD
+{
+    //http://www.codeproject.com/Articles/4416/Beginners-guide-to-accessing-SQL-Server-through-C
+    public static class DBHelper
+    {
+        //DB: DataBase
+        private static SqlConnection DB;
+        static DBHelper()
+        {
+            DB = new SqlConnection("Data Source=localhost\\SQLSERVER2012;Initial Catalog=GD1C2016;Integrated Security=True");
+        }
+
+        //SP: StoredProcedure
+        public static void ExecuteNonQuery(string SP, List<SqlParameter> parametros = null)
+        {
+            DB.Open();
+            SqlCommand command = new SqlCommand(SP, DB);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            foreach (var parametro in parametros)
+            {
+                command.Parameters.Add(parametro);
+            }
+
+            command.ExecuteNonQuery();
+            DB.Close();
+        }
+
+        public static SqlDataReader ExecuteReader(string SP, List<SqlParameter> parametros = null)
+        {
+            DB.Open();
+            SqlCommand command = new SqlCommand(SP, DB);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            foreach (var parametro in parametros)
+            {
+                command.Parameters.Add(parametro);
+            }
+
+            SqlDataReader result = command.ExecuteReader();
+            DB.Close();
+            return result;
+        }
+    }
+}

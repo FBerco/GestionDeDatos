622174988cd0cdc61503a0990965a488f94d7369
FBerco <berco.fede@gmail.com>
2016-06-08 20:10:57 -0300

    Agrego clase Rubro

---
+++ MercadoEnvio/Clases/Rubro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Rubro
+    {
+        public int Id { get; set; }
+        public string DescripcionCorta { get; set; }
+        public string DescripcionLarga { get; set; }
+    }
+}

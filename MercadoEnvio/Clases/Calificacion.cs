2757483c4006aca98fb487e6fe5ede6db4b6dd50
FBerco <berco.fede@gmail.com>
2016-06-08 19:46:31 -0300

    Agrego calificacion

---
+++ MercadoEnvio/Clases/Calificacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Calificacion
+    {
+        public int Id { get; set; }
+        public int Estrellas { get; set; }
+        public string Detalle { get; set; }
+        public DateTime Fecha { get; set; }
+        public int VentaId { get; set; }
+    }
+}

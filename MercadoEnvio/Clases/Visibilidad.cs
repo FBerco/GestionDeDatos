4fead15dd628b2ba90e46710bb7acf1858d842f3
FBerco <berco.fede@gmail.com>
2016-06-08 20:37:15 -0300

    Agrego clase Visibilidad

---
+++ MercadoEnvio/Clases/Visibilidad.cs
@@ -0,0 +1,11 @@
+﻿namespace Clases
+{
+    public class Visibilidad
+    {
+        public int Id { get; set; }
+        public string Detalle { get; set; }
+        public int PorcentajeProducto { get; set; }
+        public int CostoPublicacion { get; set; }
+        public int CostoEnvio { get; set; }
+    }
+}

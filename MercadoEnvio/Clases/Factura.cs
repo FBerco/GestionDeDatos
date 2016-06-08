f8ea1838b1337ab331178ebce55af7e7cfe7f2d3
FBerco <berco.fede@gmail.com>
2016-06-08 19:48:36 -0300

    Agrego factura

---
+++ MercadoEnvio/Clases/Factura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Factura
+    {
+        public int Numero { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Total { get; set; }
+        public int PublicacionId { get; set; }
+    }
+}

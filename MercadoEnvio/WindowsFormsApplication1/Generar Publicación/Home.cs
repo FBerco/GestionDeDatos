9ebaf4e063b1db95e4ca720890147b08b088d124
FBerco <berco.fede@gmail.com>
2016-06-14 22:19:44 -0300

    Lo muevo de lugar

--- MercadoEnvio/WindowsFormsApplication1/Generar Publicación/Home.cs
+++ MercadoEnvio/WindowsFormsApplication1/Generar Publicación/Home.cs
@@ -28,51 +28,7 @@ namespace GDD.Generar_Publicación
             CargarGrilla(DBHelper.ExecuteReader("Publicacion_GetAll").ToPublicaciones());
         }
 
-        private void CargarGrilla(List<Publicacion> publicaciones) 
-        {
-            dgvPublicaciones.DataSource = publicaciones;
-            dgvPublicaciones.Columns.Clear();
-            dgvPublicaciones.AutoGenerateColumns = false;
-
-            
-            DataGridViewTextBoxColumn Descripcion = new DataGridViewTextBoxColumn();
-            Descripcion.DataPropertyName = "Descripcion";
-            Descripcion.HeaderText = "Descripcion";
-            Descripcion.Width = 100;
-            Descripcion.ReadOnly = true;
-            DataGridViewTextBoxColumn Precio = new DataGridViewTextBoxColumn();
-            Precio.DataPropertyName = "Precio";
-            Precio.HeaderText = "Precio";
-            Precio.Width = 100;
-            Precio.ReadOnly = true;
-            DataGridViewTextBoxColumn Stock = new DataGridViewTextBoxColumn();
-            Stock.DataPropertyName = "Stock";
-            Stock.HeaderText = "Stock";
-            Stock.Width = 100;
-            Stock.ReadOnly = true;
-            DataGridViewTextBoxColumn Tipo = new DataGridViewTextBoxColumn();
-            Tipo.DataPropertyName = "Tipo";
-            Tipo.HeaderText = "Tipo";
-            Tipo.Width = 100;
-            Tipo.ReadOnly = true;
-            /*DataGridViewTextBoxColumn Usuario = new DataGridViewTextBoxColumn();
-            Usuario.DataPropertyName = "Usuario";
-            Usuario.HeaderText = "Vendedor";
-            Usuario.Width = 100;
-            Usuario.ReadOnly = true; para que aparezca el nombre del usuario*/
-            DataGridViewTextBoxColumn FechaVencimiento = new DataGridViewTextBoxColumn();
-            FechaVencimiento.DataPropertyName = "FechaVencimiento";
-            FechaVencimiento.HeaderText = "Fecha de vencimiento";
-            FechaVencimiento.Width = 100;
-            FechaVencimiento.ReadOnly = true;
-
-            //dgvPublicaciones.Columns.Add(Usuario);
-            dgvPublicaciones.Columns.Add(Descripcion);
-            dgvPublicaciones.Columns.Add(Precio);
-            dgvPublicaciones.Columns.Add(Stock);
-            dgvPublicaciones.Columns.Add(FechaVencimiento);
-            dgvPublicaciones.Columns.Add(Tipo);
-        }
+      
         
         private void btnBuscar_Click(object sender, EventArgs e)
         {
@@ -139,6 +95,52 @@ namespace GDD.Generar_Publicación
             Limpiar();
             //ActualizarGrilla();
             parametros.Clear();
+        }
+
+        private void CargarGrilla(List<Publicacion> publicaciones)
+        {
+            dgvPublicaciones.DataSource = publicaciones;
+            dgvPublicaciones.Columns.Clear();
+            dgvPublicaciones.AutoGenerateColumns = false;
+
+
+            DataGridViewTextBoxColumn Descripcion = new DataGridViewTextBoxColumn();
+            Descripcion.DataPropertyName = "Descripcion";
+            Descripcion.HeaderText = "Descripcion";
+            Descripcion.Width = 100;
+            Descripcion.ReadOnly = true;
+            DataGridViewTextBoxColumn Precio = new DataGridViewTextBoxColumn();
+            Precio.DataPropertyName = "Precio";
+            Precio.HeaderText = "Precio";
+            Precio.Width = 100;
+            Precio.ReadOnly = true;
+            DataGridViewTextBoxColumn Stock = new DataGridViewTextBoxColumn();
+            Stock.DataPropertyName = "Stock";
+            Stock.HeaderText = "Stock";
+            Stock.Width = 100;
+            Stock.ReadOnly = true;
+            DataGridViewTextBoxColumn Tipo = new DataGridViewTextBoxColumn();
+            Tipo.DataPropertyName = "Tipo";
+            Tipo.HeaderText = "Tipo";
+            Tipo.Width = 100;
+            Tipo.ReadOnly = true;
+            /*DataGridViewTextBoxColumn Usuario = new DataGridViewTextBoxColumn();
+            Usuario.DataPropertyName = "Usuario";
+            Usuario.HeaderText = "Vendedor";
+            Usuario.Width = 100;
+            Usuario.ReadOnly = true; para que aparezca el nombre del usuario*/
+            DataGridViewTextBoxColumn FechaVencimiento = new DataGridViewTextBoxColumn();
+            FechaVencimiento.DataPropertyName = "FechaVencimiento";
+            FechaVencimiento.HeaderText = "Fecha de vencimiento";
+            FechaVencimiento.Width = 100;
+            FechaVencimiento.ReadOnly = true;
+
+            //dgvPublicaciones.Columns.Add(Usuario);
+            dgvPublicaciones.Columns.Add(Descripcion);
+            dgvPublicaciones.Columns.Add(Precio);
+            dgvPublicaciones.Columns.Add(Stock);
+            dgvPublicaciones.Columns.Add(FechaVencimiento);
+            dgvPublicaciones.Columns.Add(Tipo);
         }
     }
     

70108b493b07b6560defbfc2813563df0072a437
FBerco <berco.fede@gmail.com>
2016-07-02 14:05:22 -0300

    Terminado ABM Rubro

--- MercadoEnvio/WindowsFormsApplication1/ABM Rubro/Home.Designer.cs
+++ MercadoEnvio/WindowsFormsApplication1/ABM Rubro/Home.Designer.cs
@@ -28,46 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvListadoRubros = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoRubros)).BeginInit();
+            this.cmbProductos = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cmbRubros = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // dgvListadoRubros
-            // 
-            this.dgvListadoRubros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListadoRubros.Location = new System.Drawing.Point(59, 43);
-            this.dgvListadoRubros.Name = "dgvListadoRubros";
-            this.dgvListadoRubros.Size = new System.Drawing.Size(182, 150);
-            this.dgvListadoRubros.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Location = new System.Drawing.Point(17, 118);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Rubros";
             // 
+            // cmbProductos
+            // 
+            this.cmbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProductos.FormattingEnabled = true;
+            this.cmbProductos.Location = new System.Drawing.Point(82, 51);
+            this.cmbProductos.Name = "cmbProductos";
+            this.cmbProductos.Size = new System.Drawing.Size(298, 21);
+            this.cmbProductos.TabIndex = 3;
+            this.cmbProductos.SelectedIndexChanged += new System.EventHandler(this.cmbProductos_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Productos";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(106, 182);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // cmbRubros
+            // 
+            this.cmbRubros.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRubros.FormattingEnabled = true;
+            this.cmbRubros.Location = new System.Drawing.Point(82, 118);
+            this.cmbRubros.Name = "cmbRubros";
+            this.cmbRubros.Size = new System.Drawing.Size(146, 21);
+            this.cmbRubros.TabIndex = 6;
+            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(420, 248);
+            this.Controls.Add(this.cmbRubros);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbProductos);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dgvListadoRubros);
             this.Name = "frmHome";
             this.Text = "ABM Rubro";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoRubros)).EndInit();
+            this.Load += new System.EventHandler(this.frmHome_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgvListadoRubros;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbProductos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox cmbRubros;
     }
 }
--- MercadoEnvio/WindowsFormsApplication1/ABM Rubro/Home.cs
+++ MercadoEnvio/WindowsFormsApplication1/ABM Rubro/Home.cs
@@ -5,16 +5,50 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-
+using Clases;
+using Helpers;
 using System.Windows.Forms;
 
 namespace GDD.ABM_Rubro
 {
     public partial class frmHome : Form
     {
+        private List<Publicacion> publicaciones;
+        private List<Rubro> rubros;
+        private Publicacion publiSeleccionada;
         public frmHome()
         {
             InitializeComponent();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            rubros = DBHelper.ExecuteReader("Rubro_GetAll").ToRubros();
+            cmbRubros.DataSource = rubros;
+            cmbRubros.DisplayMember = "DescripcionCorta";
+
+            publicaciones = DBHelper.ExecuteReader("Publicacion_GetAll").ToPublicaciones();
+            cmbProductos.DataSource = publicaciones;
+            cmbProductos.DisplayMember = "Descripcion";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (cmbRubros.SelectedIndex != -1)
+            {
+                var rubro = (Rubro)cmbRubros.SelectedItem;
+                DBHelper.ExecuteNonQuery("Publicacion_ModifyRubro", new Dictionary<string, object> { { "@publicacion", publiSeleccionada.Id }, { "@rubro", rubro.Id } });
+                MessageBox.Show("Modificado con exito");
+            }
+        }
+
+        private void cmbProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbProductos.SelectedIndex != -1)
+            {
+                publiSeleccionada = (Publicacion)cmbProductos.SelectedItem;
+                cmbRubros.SelectedItem = rubros.FirstOrDefault(x => x.Id == publiSeleccionada.Rubro);
+            }
         }
     }
 }

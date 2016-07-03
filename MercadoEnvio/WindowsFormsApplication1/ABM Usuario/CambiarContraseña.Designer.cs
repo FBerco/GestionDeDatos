fc45ea0c1002fe9992cdc06d1b7878c03fef1ff2
FBerco <berco.fede@gmail.com>
2016-07-03 01:00:17 -0300

    Agrego form para cambio de contraseña

---
+++ MercadoEnvio/WindowsFormsApplication1/ABM Usuario/CambiarContraseña.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GDD.ABM_Usuario
+{
+    partial class frmContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(66, 93);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(123, 42);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nueva contraseña";
+            // 
+            // frmContraseña
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 171);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "frmContraseña";
+            this.Text = "CambiarContraseña";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MercadoEnvio/WindowsFormsApplication1/ABM Usuario/CambiarContraseña.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Clases;
+using Helpers;
+using System.Windows.Forms;
+
+namespace GDD.ABM_Usuario
+{
+    public partial class frmContraseña : Form
+    {
+        private Usuario usuario;
+        public frmContraseña(Usuario us)
+        {
+            InitializeComponent();
+            usuario = us;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text != null)
+            {
+                DBHelper.ExecuteNonQuery("Usuario_CambiarContraseña", new Dictionary<string, object>() { { "@Usuario", usuario.Username}, { "@Password", txtContraseña.Text } });
+                MessageBox.Show("Contraseña cambiada");
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Escriba una contraseña");
+            }
+        }
+    }
+}

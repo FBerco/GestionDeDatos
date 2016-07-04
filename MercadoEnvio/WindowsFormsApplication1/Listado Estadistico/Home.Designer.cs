a475aa7bb77b0f445befbe8d6009b3add3109fe4
FBerco <berco.fede@gmail.com>
2016-07-04 18:32:18 -0300

    Armo los campos para que el usuario ingrese

--- MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/Home.Designer.cs
+++ MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/Home.Designer.cs
@@ -28,19 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.cmbTrimestre = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            this.cmbFiltro = new System.Windows.Forms.ComboBox();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el año";
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(126, 37);
+            this.txtAño.MaxLength = 4;
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(100, 20);
+            this.txtAño.TabIndex = 1;
+            this.txtAño.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAño_KeyPress);
+            // 
+            // cmbTrimestre
+            // 
+            this.cmbTrimestre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTrimestre.FormattingEnabled = true;
+            this.cmbTrimestre.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cmbTrimestre.Location = new System.Drawing.Point(126, 76);
+            this.cmbTrimestre.Name = "cmbTrimestre";
+            this.cmbTrimestre.Size = new System.Drawing.Size(145, 21);
+            this.cmbTrimestre.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seleccione trimestre";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Vendedores con mayor cantidad de productos no vendidos",
+            "Clientes con mayor cantidad de productos comprados",
+            "Vendedores con mayor cantidad de facturas",
+            "Vendedores con mayor monto facturado"});
+            this.cmbTipo.Location = new System.Drawing.Point(126, 105);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(300, 21);
+            this.cmbTipo.TabIndex = 4;
+            this.cmbTipo.SelectedIndexChanged += new System.EventHandler(this.cmbTipo_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Seleccione tipo";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(183, 175);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Location = new System.Drawing.Point(15, 231);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.Size = new System.Drawing.Size(451, 169);
+            this.dgvResultado.TabIndex = 7;
+            // 
+            // cmbFiltro
+            // 
+            this.cmbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFiltro.FormattingEnabled = true;
+            this.cmbFiltro.Location = new System.Drawing.Point(126, 137);
+            this.cmbFiltro.Name = "cmbFiltro";
+            this.cmbFiltro.Size = new System.Drawing.Size(121, 21);
+            this.cmbFiltro.TabIndex = 8;
+            this.cmbFiltro.Visible = false;
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(12, 140);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(108, 13);
+            this.lblFiltro.TabIndex = 9;
+            this.lblFiltro.Text = "Seleccione visibilidad";
+            this.lblFiltro.Visible = false;
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(478, 449);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.cmbFiltro);
+            this.Controls.Add(this.dgvResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbTrimestre);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.label1);
             this.Name = "frmHome";
-            this.Text = "Form1";
+            this.Text = "Listado Estadistico";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.ComboBox cmbTrimestre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView dgvResultado;
+        private System.Windows.Forms.ComboBox cmbFiltro;
+        private System.Windows.Forms.Label lblFiltro;
     }
 }
--- MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/Home.cs
+++ MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/Home.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using Clases;
+using Helpers;
 
 using System.Windows.Forms;
 
@@ -12,9 +14,94 @@ namespace GDD.Listado_Estadistico
 {
     public partial class frmHome : Form
     {
+        private int trimestre;
+        private int año;
         public frmHome()
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var tipo = cmbTipo.SelectedIndex;
+           
+            if (!string.IsNullOrEmpty(txtAño.Text) && tipo != -1 && cmbTrimestre.SelectedItem != null )
+            {
+                              
+                trimestre = ++cmbTrimestre.SelectedIndex;
+                año = Convert.ToInt32(txtAño.Text);
+                switch (tipo)
+                {
+                    case 0:
+                        var visi = (Visibilidad)cmbFiltro.SelectedItem;                        
+                        MayorNoVendido(visi);                                              
+                        break;
+                    case 1:
+                        var rubr = (Rubro)cmbFiltro.SelectedItem;
+                        MayorCantidadProductosComprados(rubr);
+                        break;
+                    case 2:
+                        MayorCantidadFActuras();
+                        break;
+                    case 3:
+                        MayorMontoFacturado();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Complete los campos por favor.");
+            }
+        }
+
+
+        private void cmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbFiltro.SelectedIndex) {
+                case 0:
+                    lblFiltro.Visible = true;
+                    cmbFiltro.DataSource = DBHelper.ExecuteReader("Visibilidad_GetAll").ToVisibilidades();
+                    cmbFiltro.DisplayMember = "Detalle";
+                    break;
+                case 1:
+                    lblFiltro.Visible = true;
+                    cmbFiltro.DataSource = DBHelper.ExecuteReader("Rubro_GetAll").ToVisibilidades();
+                    cmbFiltro.DisplayMember = "DescripcionCorta";
+                    break;
+                default:
+                    lblFiltro.Visible = false;
+                    cmbFiltro.Visible = false;
+                    break;
+            }
+        }
+
+        private void MayorNoVendido(Visibilidad visi = null)
+        {
+            
+        }
+
+        private void MayorCantidadProductosComprados(Rubro rubr)
+        {
+            throw new NotImplementedException();
+        }
+     
+        private void MayorCantidadFActuras()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MayorMontoFacturado()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void txtAño_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+                e.Handled = true;
+        }
+
     }
 }

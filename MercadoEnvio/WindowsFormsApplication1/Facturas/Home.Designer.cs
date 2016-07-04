c8b36ff2ee17bf3c68dd485a9efa65f0a2f8244f
fedejinich <jinichfede@gmail.com>
2016-07-03 22:48:52 -0300

    Empiezo con el listado de facturas.

--- MercadoEnvio/WindowsFormsApplication1/Facturas/Home.Designer.cs
+++ MercadoEnvio/WindowsFormsApplication1/Facturas/Home.Designer.cs
@@ -28,19 +28,293 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbUsuarioVendedor = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFacturas = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImporteMinimo = new System.Windows.Forms.TextBox();
+            this.txtImporteMaximo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.btnListarFacturas = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            this.txtTest = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // cmbUsuarioVendedor
+            // 
+            this.cmbUsuarioVendedor.FormattingEnabled = true;
+            this.cmbUsuarioVendedor.Location = new System.Drawing.Point(110, 26);
+            this.cmbUsuarioVendedor.Name = "cmbUsuarioVendedor";
+            this.cmbUsuarioVendedor.Size = new System.Drawing.Size(121, 21);
+            this.cmbUsuarioVendedor.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario vendedor";
+            // 
+            // lbFacturas
+            // 
+            this.lbFacturas.FormattingEnabled = true;
+            this.lbFacturas.Location = new System.Drawing.Point(293, 11);
+            this.lbFacturas.Name = "lbFacturas";
+            this.lbFacturas.Size = new System.Drawing.Size(650, 238);
+            this.lbFacturas.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fecha inicial";
+            // 
+            // txtImporteMinimo
+            // 
+            this.txtImporteMinimo.Location = new System.Drawing.Point(87, 150);
+            this.txtImporteMinimo.Name = "txtImporteMinimo";
+            this.txtImporteMinimo.Size = new System.Drawing.Size(46, 20);
+            this.txtImporteMinimo.TabIndex = 9;
+            // 
+            // txtImporteMaximo
+            // 
+            this.txtImporteMaximo.Location = new System.Drawing.Point(147, 150);
+            this.txtImporteMaximo.Name = "txtImporteMaximo";
+            this.txtImporteMaximo.Size = new System.Drawing.Size(46, 20);
+            this.txtImporteMaximo.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(135, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "$";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.BackColor = System.Drawing.Color.White;
+            this.lblMin.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lblMin.Location = new System.Drawing.Point(99, 153);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(24, 13);
+            this.lblMin.TabIndex = 13;
+            this.lblMin.Text = "Min";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.BackColor = System.Drawing.Color.White;
+            this.lblMax.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lblMax.Location = new System.Drawing.Point(157, 153);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(27, 13);
+            this.lblMax.TabIndex = 14;
+            this.lblMax.Text = "Max";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Cliente";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(87, 176);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(87, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(87, 124);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Fecha final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "FILTROS";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(955, 263);
+            this.shapeContainer1.TabIndex = 22;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 14;
+            this.lineShape1.X2 = 284;
+            this.lineShape1.Y1 = 63;
+            this.lineShape1.Y2 = 63;
+            // 
+            // btnListarFacturas
+            // 
+            this.btnListarFacturas.Location = new System.Drawing.Point(152, 226);
+            this.btnListarFacturas.Name = "btnListarFacturas";
+            this.btnListarFacturas.Size = new System.Drawing.Size(82, 23);
+            this.btnListarFacturas.TabIndex = 23;
+            this.btnListarFacturas.Text = "Listar facturas";
+            this.btnListarFacturas.UseVisualStyleBackColor = true;
+            this.btnListarFacturas.Click += new System.EventHandler(this.btnListarFacturas_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(71, 226);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 24;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(16, 4);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(75, 23);
+            this.test.TabIndex = 25;
+            this.test.Text = "test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTest
+            // 
+            this.txtTest.Location = new System.Drawing.Point(102, 0);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(100, 20);
+            this.txtTest.TabIndex = 26;
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(955, 263);
+            this.Controls.Add(this.txtTest);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnListarFacturas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtImporteMaximo);
+            this.Controls.Add(this.txtImporteMinimo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbFacturas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbUsuarioVendedor);
+            this.Controls.Add(this.shapeContainer1);
             this.Name = "frmHome";
-            this.Text = "Form1";
+            this.Text = "Listado de Facturas";
+            this.Load += new System.EventHandler(this.frmHome_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbUsuarioVendedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbFacturas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtImporteMinimo;
+        private System.Windows.Forms.TextBox txtImporteMaximo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button btnListarFacturas;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.TextBox txtTest;
     }
 }
--- MercadoEnvio/WindowsFormsApplication1/Facturas/Home.cs
+++ MercadoEnvio/WindowsFormsApplication1/Facturas/Home.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using Clases;
+using Helpers;
 
 using System.Windows.Forms;
 
@@ -12,9 +14,51 @@ namespace GDD.Facturas
 {
     public partial class frmHome : Form
     {
+        private List<Usuario>   listaDeUsuariosVendedores;
+        private List<Factura>   listaDeFacturasSegunUsuarioSeleccionado;
+        private Usuario         usuarioSeleccionado;
+        
         public frmHome()
         {
             InitializeComponent();
         }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+           listaDeUsuariosVendedores = DBHelper.ExecuteReader("Usuario_GetVendedores").ToUsuarios();
+           foreach (var usuario in listaDeUsuariosVendedores)
+           {
+               cmbUsuarioVendedor.Items.Add(usuario.Username);
+           }
+        }
+
+        private void btnListarFacturas_Click(object sender, EventArgs e)
+        {
+            usuarioSeleccionado = getUsuarioSeleccionado();
+            listaDeFacturasSegunUsuarioSeleccionado = facturasDelUsuario(usuarioSeleccionado);
+            foreach (var factura in listaDeFacturasSegunUsuarioSeleccionado) 
+            {
+                lbFacturas.Items.Add(factura);
+            }
+        }
+
+        private Usuario getUsuarioSeleccionado() 
+        {
+           return listaDeUsuariosVendedores.Find(usuario => usuario.Username == cmbUsuarioVendedor.SelectedItem);
+        }
+
+        private List<Factura> facturasDelUsuario(Usuario unUsuario) 
+        {
+            Dictionary<string, object> nuevoDiccionario = new Dictionary<string, object>();
+            nuevoDiccionario.Add("@usuario", unUsuario.Username);
+            List<Factura> facturas = DBHelper.ExecuteReader("Facturas_GetFacturasSegunUsuario", nuevoDiccionario).ToFacturas();
+            return facturas;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTest.Text = getUsuarioSeleccionado().Username;
+        }
+
     }
 }

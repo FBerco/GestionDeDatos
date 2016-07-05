46fc0d49f95dbefc66baeb459ecfa30a0aea2211
fedejinich <jinichfede@gmail.com>
2016-07-05 00:19:59 -0300

    Arreglado el helper para la clase Oferta.

--- MercadoEnvio/Clases/Oferta.cs
+++ MercadoEnvio/Clases/Oferta.cs
@@ -4,7 +4,7 @@ namespace Clases
     public class Oferta
     {
         public int Id { get; set; }
-        public double Monto { get; set; }
+        public decimal Monto { get; set; }
         public DateTime Fecha { get; set; }
         public int PublicacionId { get; set; }
         public int ClienteId { get; set; }

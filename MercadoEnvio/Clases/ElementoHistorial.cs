ba81f9194aceee6e102ebf02734c2856ec5970ce
fedejinich <jinichfede@gmail.com>
2016-07-12 17:21:53 -0300

    Creo una nueva clase para el HistorialCliente. Sirve para modelar todo el historial en una lista

---
+++ MercadoEnvio/Clases/ElementoHistorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases
+{
+    public class ElementoHistorial
+    {
+        public String Tipo { get; set; }
+        public String PublicacionID { get; set; }
+        public String Fecha { get; set; }
+        public String Cantidad { get; set; }
+        public String Monto { get; set; }
+    }
+}
